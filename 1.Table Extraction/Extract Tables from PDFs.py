58971f6125a89b7af66771854cda14f479a700e6
LoukmaneTheMaze <lokmanbezaz22@gmail.com>
2024-07-30 00:08:57 +0100

    Update Extract Tables from PDFs.py

--- 1.Table Extraction/Extract Tables from PDFs.py
+++ 1.Table Extraction/Extract Tables from PDFs.py
@@ -3,6 +3,6 @@ import camelot
 tables = camelot.read_pdf('foo.pdf', pages='1', flavor='lattice')
 print(tables)
 
-tables.export('foo.csv', f='csv', compress=True)
-tables[0].to_csv('foo.csv')  # to a csv file
+tables.export('foo.csv', f='csv', compress=True)#this exports every thing
+tables[0].to_csv('foo.csv')  # to a csv file (one table only)
 print(tables[0].df)  # to a df
